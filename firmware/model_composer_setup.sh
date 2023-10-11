1894826fe3974369dbd96e69869ce3cd7b8a08dd
Larry Ruckman <ruckman@slac.stanford.edu>
2023-10-11 14:26:49 -0700

    adding SSRDDC.slx:

---
+++ firmware/model_composer_setup.sh
@@ -0,0 +1,2 @@
+source /afs/slac.stanford.edu/g/reseng/xilinx/2023.1/Vivado/2023.1/settings64.sh
+source /afs/slac.stanford.edu/g/reseng/xilinx/2023.1/Vivado/2023.1/.settings64-model-composer.sh
---
+++ firmware/shared/model_composer/SSR_Digital_Down_Conversion/README.md
@@ -0,0 +1,3 @@
+# Template that SSRDDC.slx is copied and modified from
+https://github.com/Xilinx/Vitis_Model_Composer/blob/2023.1/Examples/HDL/RFSoC/SSR_Digital_Down_Conversion/SSRDDC.slx
+
