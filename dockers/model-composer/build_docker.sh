c470d8a00fba5606600fa4c841d47945ede2473b
Riku Nomaru <rnomaru@gmail.com>
2023-11-29 13:14:32 -0800

    fix dockerfile and build_docker.sh

--- dockers/model-composer/build_docker.sh
+++ dockers/model-composer/build_docker.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker image build . -t kek-bpm-rfsoc-model-composer-${USER}:latest --build-arg user=${USER} --build-arg uid=$(id -u)
+docker image build . -t \
+        kek-bpm-rfsoc-model-composer-${USER}:latest \
+        --build-arg user=${USER} \
+        --build-arg uid="$(id -u)" \
+        --build-arg gid="$(id -g)"
