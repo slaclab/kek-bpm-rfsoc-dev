ae5ed1449feb0ef450947976e79435064656c0b1
riku238 <rnomaru@gmail.com>
2024-05-01 00:12:05 +0900

    add datfile

--- software/scripts/makeplot_manually.py
+++ software/scripts/makeplot_manually.py
@@ -81,7 +81,7 @@ def parse_and_plot(dat_file):
         return
     if len(recordtime)==0:
         return
-
+    """
     for num in range(len(recordtime)):
         print(f'Selected time : {recordtime[num]}')
     
@@ -383,9 +383,108 @@ def parse_and_plot(dat_file):
         plt.savefig(f'/mnt/SBOR/RFSoC/{recordtime[num]}/LERUV_{recordtime[num]}_plot.png',dpi=200,bbox_inches="tight",pad_inches=0.5)
         plt.close()
         
-        
-        print("Plot saved successfully.")
-        print(f'Abort datetime:{recordtime[num]}')
+"""
+    def bunchindex(threshold,waveform):
+        start=23500
+        firstbunch=0
+        for i in range(1,8*2000):
+            if waveform[start+i-1]<waveform[start+i] and waveform[start+i]>waveform[start+i+1]:
+                if waveform[start+i]>threshold:
+                    firstbunch=start+i
+                    break
+        if firstbunch==0:
+            print("data is not invalid")
+            return firstbunch, 0
+    
+        bunch_candidate=waveform[firstbunch:firstbunch+5120*8:8]
+        bunch_index=np.where(bunch_candidate>threshold)[0]
+    
+        return firstbunch, bunch_index
+
+    firstbunch, bunch_index=bunchindex(800,ampFault[2][0])
+    if firstbunch==0:
+        print("end process")
+        return
+    os.makedirs(f'/mnt/SBOR/RFSoC/{recordtime[0]}', exist_ok=True)
+    
+    print(f'Num of bunch : {len(bunch_index)}')
+    print(f'First bunch index : {firstbunch}')
+    print(bunch_index)
+    eventnum=0
+    y_pos=[]
+    x_pos=[]
+    charge=[]
+    for j in bunch_index:
+        certain_bunch=[]
+        certain_bunch_x=[]
+        certain_bunch_charge=[]
+        for i in range(12):
+            sum_1=ampFault[2][eventnum][firstbunch+j*8+5120*8*i]
+            sum_2=ampFault[3][eventnum][firstbunch+j*8+5120*8*i]
+            sum=sum_1+sum_2
+            delta_U=ampFault[0][eventnum][firstbunch+j*8+5120*8*i+1]
+            delta_V=ampFault[1][eventnum][firstbunch+j*8+5120*8*i+1]
+            Uposition=delta_U/sum_1
+            Vposition=delta_V/sum_2
+            xposition=Uposition-Vposition
+            yposition=Uposition+Vposition
+            certain_bunch.append(yposition)
+            certain_bunch_x.append(xposition)
+            certain_bunch_charge.append(sum)
+        y_pos.append(np.array(certain_bunch))
+        x_pos.append(np.array(certain_bunch_x))
+        charge.append(np.array(certain_bunch_charge))
+    y_pos=np.array(y_pos)
+    x_pos=np.array(x_pos)
+    charge=np.array(charge)
+
+    mean=np.mean(y_pos[:,0:2],axis=1)
+    y_pos=y_pos-mean[:,np.newaxis]
+    mean=np.mean(x_pos[:,0:2],axis=1)
+    x_pos=x_pos-mean[:,np.newaxis]
+    charge_mean=np.mean(charge[:,0:2],axis=1)
+    charge=charge/charge_mean[:,np.newaxis]
+            
+    #make x axis
+    bunch_index_12=[]
+    for i in range(12):
+        bunch_index_12.append(bunch_index+5120*i)
+
+    x_axis=np.concatenate(bunch_index_12)/5120+1
+    
+    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True,figsize=(30,12))
+    split=np.hsplit(x_pos,x_pos.shape[1])
+    ax1.set_title(f'{recordtime[0]}')
+    ax1.scatter(x_axis,np.concatenate(split),color='red',s=6)
+    ax1.set_ylabel("X position (a.u,)")
+    ax1.set_ylim(-0.6,0.6)
+    ax1.grid()
+    ax1.text(0.02,0.05,'D6 Horizontal',transform=ax1.transAxes,ha='left',va='bottom',fontsize=20)
+    
+    split=np.hsplit(y_pos,y_pos.shape[1])
+    ax2.scatter(x_axis,np.concatenate(split),color='red',s=6)
+    ax2.set_ylabel("Y position (a.u.)")
+    ax2.set_ylim(-0.6,0.6)
+    ax2.grid()
+    ax2.text(0.02,0.05,'D6 Vertical',transform=ax2.transAxes,ha='left',va='bottom',fontsize=20)
+
+
+    split=np.hsplit(charge,charge.shape[1])
+    ax3.scatter(x_axis,np.concatenate(split).reshape(len(x_axis)),color='blue',s=6)
+    ax3.set_xlabel("Turn")
+    ax3.set_ylabel("Charge")
+    ax3.set_ylim(0,1.2)
+    ax3.grid()
+    ax3.set_xticks([1,2,3,4,5,6,7,8,9,10,11,12,13],['0','0','1','2','3','4','5','6','7','8','9','10','abort'])
+    ax3.set_yticks([0,0.2,0.4,0.6,0.8,1])
+    ax3.set_xlim(3,13)
+    ax3.text(0.02,0.05,'D6 Charge',transform=ax3.transAxes,ha='left',va='bottom',fontsize=20)
+
+    plt.savefig(f'/mnt/SBOR/RFSoC/{recordtime[0]}/LERD6_{recordtime[0]}_plot.png',dpi=200,bbox_inches="tight",pad_inches=0.5)
+    plt.close()
+    
+    print("Plot saved successfully.")
+    print(f'Abort datetime:{recordtime[0]}')
 
 
     
@@ -400,7 +499,7 @@ def main():
     directory_path = "../datfile"
 
     # 指定した日付
-    specified_date = datetime(2024, 4, 11, 0, 0, 0)
+    specified_date = datetime(2024, 4, 23, 12, 0, 0)
 
     # ディレクトリ内のすべての.datファイルに対して処理を実行
     for dat_file in glob.glob(os.path.join(directory_path, "*.dat")):
