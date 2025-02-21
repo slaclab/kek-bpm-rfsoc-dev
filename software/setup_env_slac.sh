61ac5050df065d0cdae31f0be4b7f53c204f459b
Larry Ruckman <ruckman@slac.stanford.edu>
2025-02-21 15:19:55 -0800

    upgrading to rogue v6.5.0

--- software/setup_env_slac.sh
+++ software/setup_env_slac.sh
@@ -6,4 +6,4 @@ source /sdf/group/faders/tools/anaconda3/etc/profile.d/conda.sh
 ##################################
 # Activate Rogue conda Environment
 ##################################
-conda activate rogue_v6.4.4
+conda activate rogue_v6.5.0
